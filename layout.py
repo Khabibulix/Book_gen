2b23e787224b497cdbef622bb942f17c9b3729b6
Khabibulix <unitetempete@gmail.com>
2022-07-19 15:59:22 +0200

    Deleting layout.py and preparing GUI in main.py for more efficiency and less troubles

--- layout.py
+++
@@ -1,2 +0,0 @@
-from Book_Chooser import Book_Chooser
-from data import next_books
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 from data import *
 from Book_Chooser import Book_Chooser
-from layout import *
+from tkinter import Tk, Button, Label
 
 if __name__ == "__main__":
 	bc = Book_Chooser()
-	bc.choosing_book()
+	
