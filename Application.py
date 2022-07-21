0ab1adbe15d36a9305be6da7dd79fedfffaa718b
Khabibulix <unitetempete@gmail.com>
2022-07-21 17:18:23 +0200

    Adding containers to discipline my elements

--- Application.py
+++ Application.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Button, Label
+from tkinter import Tk, Button, Label, Frame, Text
 from Book_Chooser import Book_Chooser
 
 bc = Book_Chooser()
@@ -7,11 +7,21 @@ class Application(Tk):
     def __init__(self):
         super().__init__()
 
-        label = Label(self, text="Let's randomize the next book I'll read -->")
+        #Top Frame
+        top_container = Frame(self, width=500, height=100, borderwidth=5, relief="groove")
+        top_container.place(x=0,y=0)
+        label = Label(top_container, text="Let's randomize the next book I'll read -->")
         label.place(x=80, y=25)
+        button = Button(top_container, text="Push me!", command=bc.choosing_book, fg="black", bg="lightblue")
+        button.place(x=320, y=23)
 
-        button = Button(self, text="Push me!", command=bc.choosing_book)
-        button.place(x=310, y=23)
+        #Bottom frame
+        bottom_container = Frame(self, width=500, height=300, borderwidth=5, relief="groove")
+        bottom_container.place(x=0, y=100)
+        # text_box = Text(self,height=12,width=40)
+        # text_box.pack(expand=True)
+        # text_box.insert("end","Yup")
+        # text_box.config(state='disabled')
 
         self.geometry("500x400")
         self.title("Book Randomizer")
