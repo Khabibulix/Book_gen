3b05c441745f5fe8bd4288445de0584cb2c51c13
Khabibulix <unitetempete@gmail.com>
2022-07-21 17:05:21 +0200

    Positionning elements inside windows

--- Application.py
+++ Application.py
@@ -7,13 +7,12 @@ class Application(Tk):
     def __init__(self):
         super().__init__()
 
-        label = Label(self, text="Let's random the next book I'll read!")
-        label.place(x=50, y=50)
-        label.pack()
+        label = Label(self, text="Let's randomize the next book I'll read -->")
+        label.place(x=80, y=25)
 
         button = Button(self, text="Push me!", command=bc.choosing_book)
-        button.pack()
+        button.place(x=310, y=23)
 
-        self.geometry("300x200")
+        self.geometry("500x400")
         self.title("Book Randomizer")
         self.resizable(False,False)
