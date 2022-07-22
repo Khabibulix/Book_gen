a37af6e879d27273748fa48b12a88246727ccb57
Khabibulix <unitetempete@gmail.com>
2022-07-22 16:42:10 +0200

    Keybinding complete for book_choosing

--- Application.py
+++ Application.py
@@ -3,21 +3,9 @@ from Book_Chooser import Book_Chooser, book_choosed
 
 bc = Book_Chooser()
 
-book_choosed = (" ").join(bc.choosing_book())
-
 class Application(Tk):
     def __init__(self):
         super().__init__()
-
-        #Top Frame
-        top_container = Frame(self, width=700, height=100, borderwidth=5, relief="groove")
-        top_container.place(x=0,y=0)
-
-        label = Label(top_container, text="Let's randomize the next book I'll read -->")
-        label.place(x=180, y=25)
-
-        button = Button(top_container, text="Push me!",command=book_choosed, fg="black", bg="lightblue")
-        button.place(x=420, y=23)
 
         #Bottom frame
         bottom_container = Frame(self, width=700, height=300, borderwidth=5, relief="groove")
@@ -25,8 +13,8 @@ class Application(Tk):
 
         text_for_result = Text(bottom_container, height=1, width=86)
         text_for_result.pack(expand=True)
-        text_for_result.insert("end", book_choosed)
-        text_for_result.config(state='disabled')
+        text_for_result.config(state='normal')
+
 
         text_box_for_url = Text(bottom_container, height=1, width=86)
         text_box_for_url.pack(expand=True)
@@ -37,6 +25,23 @@ class Application(Tk):
         text_box_for_informations.pack(expand=True)
         text_box_for_informations.insert("end","Contenu des infos ici")
         text_box_for_informations.config(state='disabled')
+
+        # Top Frame
+        top_container = Frame(self, width=700, height=100, borderwidth=5, relief="groove")
+        top_container.place(x=0, y=0)
+
+        label = Label(top_container, text="Let's randomize the next book I'll read -->")
+        label.place(x=180, y=25)
+
+        button = Button(top_container, text="Generate!",fg="black", bg="lightblue")
+        button.place(x=420, y=23)
+
+        button_delete = Button(top_container, text="Delete!", fg="black", bg="lightblue")
+        button_delete.place(x=500, y=23)
+
+        #Keybindings
+        button.bind('<Button-1>', lambda event: text_for_result.insert("end", bc.choosing_book()))
+        button_delete.bind('<Button-1>', lambda event: text_for_result.delete("1.0", "end"))
 
         self.geometry("700x400")
         self.title("Book Randomizer")
