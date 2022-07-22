f933edc2ea2ce506a02affe5616aa9da28d6cf2e
Khabibulix <unitetempete@gmail.com>
2022-07-22 16:57:06 +0200

    Removing mood feature

--- Book_Chooser.py
+++ Book_Chooser.py
@@ -9,37 +9,6 @@ url = ""
 infos = ""
 
 class Book_Chooser:
-    #
-    # def extract_mood(self):
-    #     """
-    #     List comprehension to get all the moods in data.py
-    #     :return: moodlist converted to string
-    #     """
-    #     mood_list = []
-    #     for i in next_books:
-    #         if i["mood"] not in mood_list:
-    #             mood_list.append(i["mood"])
-    #     return (",").join(mood_list)
-    #
-    # def choosing_mood_if_user_consentment_or_print_a_random_book(self):
-    #     """
-    #     The user  inputs a mood which will be used to choose a book later
-    #     :return: mood which is a string
-    #     """
-    #     yes_or_no = input("Voulez-vous choisir selon une humeur? \t oui/non\n")
-    #     if yes_or_no.lower() == "oui":
-    #         print("Humeurs disponibles:", self.extract_mood())
-    #         while True:
-    #             mood = input("De quelle humeur es-tu en ce moment ? ")
-    #             if mood.isalpha():
-    #                 if mood in self.extract_mood():
-    #                     return mood
-    #                 else:
-    #                     print("Cette humeur n'est pas valide")
-    #                     continue
-    #             else:
-    #                 print("Veuillez n\'entrer que des lettres")
-    #     return -1
 
     def choosing_from_list(self):
         """
@@ -54,12 +23,6 @@ class Book_Chooser:
         Main function that calls all the others
         :return: book_choosed which is a data humanly readable for output
         """
-        #mood = self.choosing_mood_if_user_consentment_or_print_a_random_book()
-        # if mood != -1:
-        #     next_books_sorted = [x for x in next_books if x["mood"] == mood]
-        #     item_choosed = random.choice(next_books_sorted)
-        #     print("\nDans ce cas, je te conseille:",item_choosed["titre"], "écrit par", item_choosed["auteur"])
-        # else:
         item_choosed = self.choosing_from_list()
         book_choosed = str(item_choosed["titre"]), "par", str(item_choosed["auteur"])
         self.printing_informations_about_book(item_choosed["titre"], item_choosed["auteur"])
